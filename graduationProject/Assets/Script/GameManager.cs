3905c4e8187444146589997dd358248a63afb8d1
misakila27 <misakila58@gmail.com>
2020-09-28 10:14:52 +0900

    전체 프로젝트 업로드

---
+++ graduationProject/Assets/Script/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PickGame(int gameNum) //1: 잔치기 
+    {
+        switch (gameNum)
+        {
+            case 1:
+                break;
+
+            case 2:
+                break;
+            default:
+                break;
+
+        }
+
+    }
+}
---
+++ graduationProject/Assets/Script/Player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    bool is_player;
+    bool myTurn;
+    GameManager gameManager;
+    Queue q_player = new Queue();
+    public int p_turn;
+    public GameObject[] player = new GameObject[5];
+    public GameObject Arrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void ChangeTurn()
+    {
+        p_turn++;
+ 
+        
+        switch (p_turn)
+        {
+    
+            case 1:
+                Vector3 pos = player[p_turn].transform.position;
+                Arrow.transform.position = new Vector3(pos.x,pos.y +50,0);
+                break;
+            case 2:
+                pos = player[p_turn].transform.position;
+                Arrow.transform.position = new Vector3(pos.x, pos.y + 50, 0);
+                break;
+            case 3:
+                pos = player[p_turn].transform.position;
+                Arrow.transform.position = new Vector3(pos.x, pos.y + 50, 0);
+                break;
+            case 4:
+                pos = player[p_turn].transform.position;
+                Arrow.transform.position = new Vector3(pos.x, pos.y + 50, 0);
+                break;
+            case 5:
+                p_turn = 1;
+                goto case 1;
+              
+            default:
+                break;
+        }
+
+
+
+    }
+}
